db2301ee2e5f29d418f0714fe2ceb67ec311cd52
Samu Rahkila <samu.rahkila@gmail.com>
2021-03-14 14:22:54 +0200

    v1.3 - added some randomised enemy properties, added health recovery upon defeating an enemy, changed variable 'enemyCount' to more understandable 'winCount' and added logic to give player bonus health for every 2 defeated enemies, added console log message 'You attack the (enemy)...' to make the beginning of a turn more obvious in the Console

--- SIMPLE_RPG/version1-2.js
+++ SIMPLE_RPG/version1-2.js
@@ -1,4 +1,4 @@
-// Object Test v1.2
+// Object Test v1.3
 // The initial console.log messages are after the Objects are defined because otherwise it will give an error 'Can not access [X] before intialization. Maybe there is some work-around; maybe it doesn't really matter.
 // *****************************************************************************************************************************************************
 // Bugs and other stuff we don't want:
@@ -9,9 +9,9 @@
 // -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-// *************************************************** Initializing characters for player and enemies ***************************************************
+// *************************************************** Initializing characters for player and enemies + player methods ***************************************************
 
-let enemyCount = 1; // To track victories for example.
+let winCount = 0;
 
 let enemy;
 
@@ -26,11 +26,11 @@ class Character {
 
 function spawnEnemy() {
     let randomNum = Math.random();
-    if (randomNum <= 0.4) { // 40% chance to encounter Master Swordsman.
-        enemy = new Character ("Master Swordsman", 7, 6, 1);
+    if (randomNum <= 0.45) { // 45% chance to encounter Master Swordsman.
+        enemy = new Character ("Master Swordsman", (Math.ceil(Math.random()*4) + 4), 6, (Math.round(Math.random()) + 1)); // Some property values randomised.
         Character.prototype.attack = swordsmanStrike; // Adds the specific function as a method for the Character.
     } else {
-        enemy = new Character ("Common Knight", 10, 5, 0);
+        enemy = new Character ("Common Knight", (10 + (Math.round(Math.random()))), (Math.ceil(Math.random()*2) + 3), (Math.round(Math.random())));
         Character.prototype.attack = knightSlash;
     }
 }
@@ -41,12 +41,19 @@ let player = {
     damage: 5,
     defense: 3,
     attack: function () {
+        console.log(`You attack the ${enemy.id}...`)
         enemy.health = enemy.health - (this.damage - enemy.defense);
         if (enemy.health >= 1) {
             console.log(`You dealt ${this.damage - enemy.defense} damage! ${enemy.id} HP: ${enemy.health}`);
         } else {
-            console.log(`You dealt ${this.damage - enemy.defense} damage and ${enemy.id} was defeated! Total victories: ${enemyCount}`);
-            enemyCount++;
+            winCount++;
+            console.log(`You dealt ${this.damage - enemy.defense} damage and ${enemy.id} was defeated! Total victories: ${winCount}`);
+            this.health = this.health + (Math.round(enemy.damage / 3));
+            console.log(`You recovered ${(Math.round(enemy.damage / 3))} health! Your HP: ${this.health}`); // Every enemy defeated gives you some health back.
+            if (winCount % 2 == 0) {
+                this.health = this.health + 3; // Every two enemies you defeat gives you bonus health.
+                console.log(`Your battle experience defeating multiple foes has made you stronger; you gained 3 HP! Your HP: ${this.health}`);
+            }
             spawnEnemy(); // Creating a new enemy after the previous is defeated.
             console.log(`A new ${enemy.id} appeared! ||HP: ${enemy.health}|| ||DMG: ${enemy.damage}|| ||DEF: ${enemy.defense}||`);
         }
@@ -57,7 +64,7 @@ spawnEnemy(); // Creating the first enemy.
 
 // *************************************************** Beginning console messages ***************************************************
 
-console.log('Object Test v1.2');
+console.log('Object Test v1.3');
 
 console.log(`You are playing as a ${player.id}, ||HP: ${player.health}|| ||DMG: ${player.damage}|| ||DEF: ${player.defense}||`);
 
