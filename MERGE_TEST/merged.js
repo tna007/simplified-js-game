38ad09c290d311dda18417d9a9a4a774253d80d2
Samu Rahkila <samu.rahkila@gmail.com>
2021-03-22 19:54:15 +0200

    2nd button merged into code successfully

--- MERGE_TEST/merged.js
+++ MERGE_TEST/merged.js
@@ -2,7 +2,7 @@
 
 // ************************************* Fetch data and create Character objects *************************************
 (async function getData() {
-    const TARGET_JSON = "https://public.bc.fi/s2100145/mergeJSONfile/merged.json"; // our own .json file.
+    const TARGET_JSON = "https://raw.githubusercontent.com/tna007/simplified-js-game/fetch_villain/test.json"; // our own .json file.
 
     let response = await fetch(`${TARGET_JSON}`);
     let json = await response.json();
@@ -13,20 +13,24 @@
     let winCount = 0;
 
     class Character {
-        constructor(id, health, damage, defense, imageurl, whichFunc) {
+        constructor(id, health, damage, defense, imageurl, whichFunc, ultra) {
             this.id = id;
             this.health = health;
             this.damage = damage;
             this.defense = defense;
             this.imageurl = imageurl;
             this.whichFunc = whichFunc; // variable that is passed into getMethod to acquire correct function from methodArray.
+            this.ultra = ultra; // secondary function.
         }
         getMethod(x) {
             methodArray[x](); // when a Character class object calls getMethod, it will execute a function from methodArray based on the parameter that is passed.
         }
+        getUltra(x) {
+            methodUltraArray[x]();
+        }
     }
 
-    let player = new Character("Hero", 10, 6, 1, "https://public.bc.fi/s2100145/simpleRPGsprites/hero.png.png", 5); // note: whichFunc, the last value, will need to be changed depending on which index in methodArray the player's attack function is.
+    let player = new Character("Hero", 10, 6, 1, "https://public.bc.fi/s2100145/simpleRPGsprites/hero.png.png", 5, 0); // note: whichFunc, the last value, will need to be changed depending on which index in methodArray the player's attack function is.
 
     let enemy = new Character(json.enemies[randomNum].id, json.enemies[randomNum].health, json.enemies[randomNum].damage, json.enemies[randomNum].defense, json.enemies[randomNum].imageurl, json.enemies[randomNum].whichFunc); // first enemy created.
 
@@ -146,12 +150,38 @@
         }
     }
 
+    function heroUltra() {
+        console.log(`This is the ultra move!!`);
+        enemy.health = enemy.health - player.damage;
+        if (enemy.health >= 1) {
+            console.log(`You dealt ${player.damage} damage! ${enemy.id} HP: ${enemy.health}`);
+        } else {
+            winCount++;
+            console.log(`Ultra move! ${enemy.id} was defeated! Total victories: ${winCount}`);
+            player.health = player.health + (Math.round(enemy.damage / 3));
+            console.log(`You recovered ${(Math.round(enemy.damage / 3))} health! Your HP: ${player.health}`); // Every enemy defeated gives you some health back.
+            if (winCount % 2 == 0) {
+                player.health = player.health + 3; // Every two enemies you defeat gives you bonus health.
+                console.log(`Your battle experience defeating multiple foes has made you stronger; you gained 3 HP! Your HP: ${player.health}`);
+            }
+            newEnemy(); // create a new enemy after defeating the previous one.
+            console.log(`New enemy ${enemy.id} appeared! HP: ${enemy.health} DMG: ${enemy.damage} DEF: ${enemy.defense}`);
+        }
+    }
+
     let methodArray = [ogreAttack, skeleAttack, wereAttack, demonAttack, artoriasAttack, heroAttack]; // functions to be used by the objects as methods.
+
+    let methodUltraArray = [heroUltra]; // secondary function.
 
 // ************************************* Button functions *************************************
 
     document.getElementById("turn").addEventListener("click", function takeTurn(){
         player.getMethod(player.whichFunc);
+        enemy.getMethod(enemy.whichFunc);
+    });
+
+    document.getElementById("ultra").addEventListener("click", function takeTurn(){
+        player.getUltra(player.ultra);
         enemy.getMethod(enemy.whichFunc);
     });
 
